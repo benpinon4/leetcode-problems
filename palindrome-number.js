b0bd6ec2ce4933de707f6d7c66ab58e90b9d62f1
benpinon4 <benjamin.pinon@CodeImmersives.com>
2025-04-16 11:11:45 -0400

    refactoring

--- palindrome-number.js
+++ palindrome-number.js
@@ -3,17 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  let intToStr = x.toString();
-  let splitInteger = intToStr.split("");
-  let reversedIntArr = [];
+  let rev =  parseInt(x.toString().split("").reverse().join(""))
+//   let reversedInteger1 = parseInt();
+//   let splitInteger = intToStr.split("");
+//   let reversedIntArr = [];
 
-  for (let index = splitInteger.length - 1; index > -1; index--) {
-    reversedIntArr = [...reversedIntArr, splitInteger[index]];
-  }
-  let newArr = reversedIntArr.join("");
-  let reversedInteger = parseInt(newArr);
+//   for (let index = splitInteger.length - 1; index > -1; index--) {
+//     reversedIntArr = [...reversedIntArr, splitInteger[index]];
+//   }
+//   let newArr = reversedIntArr.join("");
+//   let reversedInteger = parseInt(newArr);
 
-  return x == reversedInteger;
+  return x == rev;
 };
 
 console.log(isPalindrome(121))
